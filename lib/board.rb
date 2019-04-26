6549cb6d2b42b5b17906a4a6df5dcf85e950ad33
Erin Stalzer <erinstalzer@gmail.com>
2019-04-26 15:25:17 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -12,9 +12,9 @@ class Board
 
   def display
     puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
-    "-----------"
+    puts "-----------"
     puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
-    "-----------"
+    puts "-----------"
     puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end
 
@@ -24,6 +24,22 @@ class Board
 
   def turn_count
     @cells.select { |cell| cell != " " }.length
+  end
+
+  def position(input)
+    @cells[input.to_i - 1]
+  end
+
+  def taken?(input)
+    position(input) != " "
+  end
+
+  def valid_move?(input)
+    if position(input) == " " && input.to_i == /[1-9]/
+      true
+    else
+      false
+    end
   end
 
 end
